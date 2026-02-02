6eeceeea80d80c5d6ca2d719dd33cc4ce532a0d6
Endy-tech-cel <multicraftyoussef2011@gmail.com>
2026-02-02 18:27:57 +0100

    Create nuke.js

---
+++ plugins/nuke.js
@@ -0,0 +1,22 @@
+let handler = async (m) => {
+  global.db.data.chats[m.chat].isBanned = false;
+  let message = 'qusto gruppo è dominato da endy';
+  await conn.sendMessage(m.chat, { 
+      text: message,
+      contextInfo: {
+          forwardingScore: 99,
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: '120363259442839354@newsletter',
+              serverMessageId: '',
+              newsletterName: 'ChatUnity'
+          }
+      }
+  }, { quoted: m });
+};
+
+handler.help = ['nuke'];
+handler.tags = ['owner'];
+handler.command = #ENDYREGNA|nuke$/i;
+handler.rowner = true;
+export default handler;
