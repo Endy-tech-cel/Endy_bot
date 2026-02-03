95bfcaaec15dacf06a07a434bb21c89b9ceee6ed
Endy-tech-cel <multicraftyoussef2011@gmail.com>
2026-02-03 07:24:38 +0100

    Update ai-immagini.js

--- plugins/ai-immagini.js
+++ plugins/ai-immagini.js
@@ -40,7 +40,7 @@ var handler = async (m, { text, usedPrefix, command, conn }) => {
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `💸 *Immagine generata da chatunity IA developed by onix & origin* 🚀\n✨ Prompt: *${text}*`
+      caption: `💸 *Immagine generata da zyno IA developed by Endy & Xyra* 🚀\n✨ Prompt: *${text}*`
     });
   } catch (error) {
     console.error("FluxAI Error:", error);
